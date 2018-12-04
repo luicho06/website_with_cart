a6066797f6fc37c7a0a177dbe4f73e4028171253
luicho06 <40962190+luicho06@users.noreply.github.com>
2018-12-03 19:04:28 -0500

    Add files via upload

---
+++ luicho's portfolio/Gallery.php
@@ -0,0 +1,46 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="main.css">
+<title>Luicho's portfolio</title>
+</head>
+    <body>
+    <header> <div class="container">
+        <div id="branding"><h1>Luicho.com</h1>
+        </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>  
+            <li><a href="Gallery.php">Gallery</a></li>
+          
+            <li>cart</li>
+        </ul>
+    </nav>
+        </div>
+        </header>        
+<main>
+        
+       <h1>3D OBJECTS</h1>
+       <H2>CANON</H2>
+       <img src="_010001.png" width="500" height="300">
+       <button>ADD TO CART</button> 
+       
+       <h2>kid cartoon</h2>
+        <img src="muneco.png" width="500" height="300">
+        <button>ADD TO CART</button>
+        
+       	<h2>pill for games</h2>
+        <img src="pill_health.png" width="500" height="300">
+        <button>ADD TO CART</button>
+        
+        
+
+        
+</main>
+        
+<footer></footer>        
+        
+    </body>    
+</html>    
---
+++ luicho's portfolio/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="main.css">
+<title>Luicho's portfolio</title>
+</head>
+    
+ 
+    <body>
+    <header> <div class="container">
+        <div id="branding"><h1>Luicho.com</h1>
+        </div>
+    <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+              
+            <li><a href="Gallery.php">Gallery</a></li>    
+       
+            <li>Cart</li> 
+        </ul>
+    </nav>
+        </div>
+        
+        </header>         
+<main>
+   
+    
+    <div id="showcase"><h1>Welcome!!!</h1><p>I'm full of creative ideas!!!</p></div>
+    
+    
+            <div id="side">
+            <h3>Jump to:</h3>    
+             <ul>
+                 <a href="#draw"><div id="drawing"><li>Drawing</li></div></a>
+                 <a href="#ani"><div id="anime"><li>3D/2D animation and modeling</li></div></a>
+                 <a href="#musiccration">  <div id="music"><li>Music Creation</li></div></a>
+                 <a href="#gamedesign">  <div id="game"><li>Game Creation</li></div></a>
+                 <a href="#code">  <div id="webdev"><li>Web Development</li></div></a>
+                 <li>Cart</li>
+    
+    </ul>
+    
+            </div>    
+            
+    
+    
+        <div id="content">  
+            
+       <div id="intro"> <h1> Hello everyone</h1>
+        
+           <p>Thanks to visit my website. This site aims to give information to visitors and potencial employers about me and what I'm able to do.</p>
+            <p>In this website you will see my work and how I explode my creativity to the space by looking at my portfolio which range from drawing to music and even 3D graphics.</p>
+           <p>Below there are some topics that my work is based on</p>
+            
+            
+            
+            
+            </div>
+    
+            
+            
+            
+    <hr/>
+    <h1> Things that I'm good at:</h1>
+
+    <h2 id="draw">Drawing</h2> 
+    
+            <img src="images/drawings1.jpg" alt="drawing">
+    
+    <p> </p>
+    
+    <hr/>
+    
+    <h2 id="ani">3D and 2D modelling/ Animation </h2>
+    
+    
+    
+    <p> I'm not even near to be a professional at these, but I'm currently learning new things every single day and I love it!</p>
+            
+     <img src="images/preview1.jpg" alt="character creation">
+  
+    
+    <hr/>
+    
+    <h2 id="musiccration">Music Creation</h2>
+    
+        <img src="images/music3.png" alt="music creation">
+
+    
+    <p> Just like 3D modelling and animation I am not a professional in this. But I am learning new things and I can brag that I have done some projects</p>
+    
+    <hr/>
+    
+    
+    <h2 id="gamedesign">Game Creation</h2>
+    
+     <img src="images/untitled.png" alt="game model">
+
+    
+    <p>I know basic knowledge of game creation. Currently learning</p>
+    
+    <hr/>
+    
+    <h2 id="code">Coding</h2>
+    
+    <img src="images/webdev3.jpg" alt="web development">
+
+    
+    <p>Basic knowledge of web creation. Currently learning</p>
+    
+    <hr/>
+    
+    </div>
+</main>
+        
+    
+        <footer><p>Luicho's portfolio, copyright &copy; 2017</p></footer>        
+        
+    </body>    
+</html>    
